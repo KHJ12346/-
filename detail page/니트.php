d6e7b9f8e825697c4ebddd3ef6cd3b7017b222a5
JangSeoyeong <81270684+JangSeoyeong2@users.noreply.github.com>
2023-05-01 16:20:38 +0900

    판매 페이지/미완성본

---
+++ detail page/니트.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>상품 상세 페이지</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+      .product {
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body class="container">
+    <header>
+      <h1>상품 상세 페이지</h1>
+      <nav>
+      </nav>
+    </header>
+    <main>
+      <div class="product">
+        <form action="checkout.php" method="post">
+          <p id="stock-msg"></p>
+        </form>
+        <h3>가격: $25.00 </h3>
+        <h2>상품 설명</h2>
+        <img src="니트.jpg" alt="상품 이미지">
+      </div>
+    </main>
+  </body>
+</html>
---
+++ detail page/패딩.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>상품 상세 페이지</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+      .product {
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body class="container">
+    <header>
+      <h1>상품 상세 페이지</h1>
+      <nav>
+      </nav>
+    </header>
+    <main>
+      <div class="product">
+        <form action="checkout.php" method="post">
+        </form>
+        <h3>가격: $280.00 </h3>
+        <h2>상품 설명</h2>
+        <img src="패딩.jpg" alt="상품 이미지">
+      </div>
+    </main>
+  </body>
+</html>
